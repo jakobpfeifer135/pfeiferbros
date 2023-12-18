bcab6a70f0178d61831287cc7ac17108bf955a18
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-12-18 13:21:35 -0600

    troubleshoot

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import PostcssNested from 'postcss-nested';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), PostcssNested()],
   server: {
     port: 3002,
     open: true
