4f21acaa92eac955f7219ebeeb4d754f59ba0273
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-12-19 12:35:51 -0600

    commented out backend

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+// const mongoose = require('mongoose');
+// require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || process.env.CONN_STRING);
+// mongoose.connect(process.env.MONGODB_URI || process.env.CONN_STRING);
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
