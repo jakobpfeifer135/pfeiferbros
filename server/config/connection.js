6399e1e89520851a4539f6f6579a765d2ab2b2cb
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-12-19 13:23:42 -0600

    restructure

--- server/config/connection.js
+++
@@ -1,6 +0,0 @@
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-
-// mongoose.connect(process.env.MONGODB_URI || process.env.CONN_STRING);
-
-// module.exports = mongoose.connection;
