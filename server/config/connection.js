7ca1c6cd0979e0014e8ca0598b27b564d0d69e7b
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-12-18 18:06:58 -0600

    added gitignore

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pfeifer_bros');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pfeiferbros');
 
 module.exports = mongoose.connection;
